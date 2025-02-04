5ce354205ee32ad3ba418bc8a50e5d0b021fdbe3
HobbesUnleashed <sleacy@live.co.uk>
2025-02-04 14:41:13 +0000

    Bootstrap basics - COMPLETED

---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '286642fe9448d78d5450d894fb8a649f0541a24a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '286642fe9448d78d5450d894fb8a649f0541a24a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v4.1.3',
+            'version' => '4.1.3.0',
+            'reference' => '3b558734382ce58b51e5fc676453bfd53bba9201',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v4.1.3',
+            ),
+        ),
+    ),
+);
